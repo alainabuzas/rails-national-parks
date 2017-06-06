44bf801dbe26745aa843ba1e745977a5bd3982a0
alainabuzas <alaina.buzas@gmail.com>
2017-06-06 15:32:48 -0700

    create rangers views controller model

--- app/models/park.rb
+++ app/models/park.rb
@@ -1,2 +1,5 @@
 class Park < ApplicationRecord
+
+	has_and_belongs_to_many :rangers
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,35 @@
 Rails.application.routes.draw do
+  # get 'rangers/index'
+
+  # get 'rangers/show'
+
+  # get 'rangers/new'
+
+  # get 'rangers/edit'
+
   get 'home/index'
 
   get 'home/about'
 
   root 'home#index'
 
-  get 'parks/index'
+  # get 'parks/index'
 
-  get 'parks/new'
+  # get 'parks/new'
 
-  get 'parks/edit'
+  # get 'parks/edit'
 
-  get 'parks/show'
+  # get 'parks/show'
 
   # post 'parks/create'
 
   # put 'parks/update'
 
-  delete 'parks/destroy'
+  # delete 'parks/destroy'
 
   resources :parks
+
+  resources :rangers
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
